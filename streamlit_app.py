fca2d7292b294f07fac839222f8e6b8b4540c14f
mayuriabhandari-max <mayuri.a.bhandari@gmail.com>
2026-02-24 17:16:06 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(page_title="RAG Chatbot")
+
+st.title("📚 RAG Chatbot")
+st.write("Welcome Mayuri 👋")
+
+question = st.text_input("Ask a question:")
+
+if question:
+    st.write("You asked:", question)
